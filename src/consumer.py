fd073938740b9953dfe4bd7783fc25ec0de518d9
Leonardo Agustoni <leonardo.agustoni@sap.com>
2024-05-17 12:27:20 -0300

    chore: fix pylint issues

--- src/consumer.py
+++ src/consumer.py
@@ -1,30 +1,58 @@
-import paho.mqtt.client as mqtt
 import json
 import sqlite3
+import paho.mqtt.client as mqtt
 
-mqtt_broker_ip = "broker.hivemq.com"  # Replace with your MQTT broker IP
-mqtt_port = 1883
-topic = "unisinos/feilke/weather_station/data"
-database_file = "weather_station.db"  # SQLite Database file
+MQTT_BROKER_IP = "broker.hivemq.com"  # Replace with your MQTT broker IP
+MQTT_PORT = 1883
+TOPIC = "unisinos/feilke/weather_station/data"
+DATABASE_FILE = "weather_station.db"  # SQLite Database file
+
 
 def on_connect(client, __userdata__, __flags__, rc, __properties__):
-    print("Connected with result code "+str(rc))
-    client.subscribe(topic)
+    """Handle the on_connect event from the MQTT broker
+
+    Args:
+        client (_type_): The client instance for this callback
+        __userdata__ (_type_): _description_
+        __flags__ (_type_): _description_
+        rc (_type_): Result code from the connection
+        __properties__ (_type_): _description_
+    """
+    print("Connected with result code " + str(rc))
+    client.subscribe(TOPIC)
+
 
 def on_message(__client__, __userdata__, msg):
-    message_json = msg.payload.decode('utf-8')
+    """Handle the on_message event from the MQTT broker
+
+    Args:
+        __client__ (_type_): _description_
+        __userdata__ (_type_): _description_
+        msg (_type_): The message received from the MQTT broker
+    """
+
+    message_json = msg.payload.decode("utf-8")
     message = json.loads(message_json)
-    temperature = message['temperature']
-    humidity = message['humidity']
-    wind_speed = message['wind_speed']
-    print(f"Analyzing weather data. Temperature: {temperature}, Humidity: {humidity}, Wind Speed: {wind_speed}")
+    temperature = message["temperature"]
+    humidity = message["humidity"]
+    wind_speed = message["wind_speed"]
+    print(f"Analyzing data. T: {temperature}, H: {humidity}, WS: {wind_speed}")
 
     # Save to SQLite database
-   
-    conn = sqlite3.connect(database_file)
+
+    conn = sqlite3.connect(DATABASE_FILE)
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS WeatherData (temperature real, humidity real, wind_speed real)')
-    insert_query = f"INSERT INTO WeatherData (temperature, humidity, wind_speed) VALUES ({temperature}, {humidity}, {wind_speed})"
+    c.execute(
+        """
+        CREATE TABLE IF NOT EXISTS WeatherData 
+        (temperature real, humidity real, wind_speed real)
+        """
+    )
+    insert_query = f"""
+    INSERT INTO WeatherData 
+    (temperature, humidity, wind_speed) 
+    VALUES ({temperature}, {humidity}, {wind_speed})"""
+
     c.execute(insert_query)
     conn.commit()
     conn.close()
@@ -34,6 +62,6 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "WeatherStationConsumer")
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(mqtt_broker_ip, port=mqtt_port)
+client.connect(MQTT_BROKER_IP, port=MQTT_PORT)
 
 client.loop_forever()
--- src/producer.py
+++ src/producer.py
@@ -1,17 +1,28 @@
-import paho.mqtt.client as mqtt
 import time
 import random
 import json
+import paho.mqtt.client as mqtt
 
-mqtt_broker_ip = "broker.hivemq.com"  # Replace with your MQTT broker IP
-mqtt_port = 1883
-topic = "unisinos/feilke/weather_station/data"
+MQTT_BROKER_IP = "broker.hivemq.com"  # Replace with your MQTT broker IP
+MQTT_PORT = 1883
+TOPIC = "unisinos/feilke/weather_station/data"
+
 
 def on_connect(__client__, __userdata__, __flags__, rc, __properties__):
+    """Handles the on_connect event from the MQTT broker
+
+    Args:
+        __client__ (_type_): _description_
+        __userdata__ (_type_): _description_
+        __flags__ (_type_): _description_
+        rc (_type_): The result code from the connection
+        __properties__ (_type_): _description_
+    """
     if rc == 0:
         print("Connected to MQTT broker!")
     else:
         print("Failed to connect, return code %d\n", rc)
+
 
 # Create new instance
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "WeatherStationProducer")
@@ -19,7 +30,7 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "WeatherStationProducer")
 client.on_connect = on_connect
 
 # Connecting to broker
-client.connect(mqtt_broker_ip, port=mqtt_port)
+client.connect(MQTT_BROKER_IP, port=MQTT_PORT)
 
 client.loop_start()
 
@@ -28,10 +39,14 @@ try:
         temperature = round(random.uniform(18.0, 28.0), 2)
         humidity = round(random.uniform(40, 60), 2)
         wind_speed = round(random.uniform(2, 10), 2)
-        message = {"temperature": temperature, "humidity": humidity, "wind_speed": wind_speed}
+        message = {
+            "temperature": temperature,
+            "humidity": humidity,
+            "wind_speed": wind_speed,
+        }
         message_json = json.dumps(message)
-        client.publish(topic, message_json)
-        print(f"Published message {message} to topic {topic}")
+        client.publish(TOPIC, message_json)
+        print(f"Published message {message} to topic {TOPIC}")
         time.sleep(1)
 
 except KeyboardInterrupt:
